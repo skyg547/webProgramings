27d54aa1267936158faad10016d4206f7ef9cba8
skyg547 <44772145+skyg547@users.noreply.github.com>
2019-12-30 00:13:10 +0900

    공부1

---
+++ WebContent/ch01.js
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/*
+var a = "i love boostcourese";
+
+a;
+"i love boostcourse"
+
+var a ="i love boostcourse2";
+
+a;
+"i love boostcourse2";*/
+
+const name = "crong";
+
+const myname = name || "d";
